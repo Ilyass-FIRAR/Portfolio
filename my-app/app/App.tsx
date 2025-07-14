fe77db05bba8086219139f726174318d1eca0a6e
charli <ilyass.firar88@gmail.com>
2025-07-14 11:25:27 +0100

    dwas

---
+++ my-app/app/App.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import Navigation from './components/Navigation';
+import Hero from './components/Hero';
+import About from './components/About';
+import Skills from './components/Skills';
+import Projects from './components/Projects';
+import Contact from './components/Contact';
